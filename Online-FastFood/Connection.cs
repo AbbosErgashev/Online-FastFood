53894217f23cead84f4441ac6b3f8d9db8016df8
eaxusniddinovich <eaxusniddinovich@gmail.com>
2024-05-27 00:38:38 +0500

    add admin fields

---
+++ Online-FastFood/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+
+namespace Online_FastFood
+{
+    public class Connection
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        }
+
+        public class Utils
+        {
+            public static bool IsValidExtension(string fileName)
+            {
+                bool isValid = false;
+                string[] fileExtension = { ".jpg", ".png", ".jpeg" };
+                for (int i = 0; i < fileExtension.Length - 1; i++)
+                {
+                    if (fileName.Contains(fileExtension[i]))
+                    {
+                        isValid = true;
+                        break;
+                    }
+                }
+
+                return isValid;
+            }
+        }
+    }
+}
