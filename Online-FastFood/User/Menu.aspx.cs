e293013bd75169b6822a5f9874868945506467c0
eaxusniddinovich <eaxusniddinovich@gmail.com>
2024-06-01 12:51:10 +0500

    added menu items. added data-id in custom.js

--- Online-FastFood/User/Menu.aspx.cs
+++ Online-FastFood/User/Menu.aspx.cs
@@ -1,17 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+﻿using Online_FastFood.Admin;
+using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Online_FastFood.User
 {
     public partial class Menu : System.Web.UI.Page
     {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                getCategories();
+                getProducts();
+            }
         }
+
+        private void getCategories()
+        {
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("Category_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", "activecat");
+            cmd.CommandType = CommandType.StoredProcedure;
+            sda = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sda.Fill(dt);
+            rCategory.DataSource = dt;
+            rCategory.DataBind();
+        }
+
+        private void getProducts()
+        {
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("Product_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", "activeprod");
+            cmd.CommandType = CommandType.StoredProcedure;
+            sda = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sda.Fill(dt);
+            rProducts.DataSource = dt;
+            rProducts.DataBind();
+        }
+
+        public string LoverCase(object obj)
+        {
+            return obj.ToString().ToLower();
+        }
+
     }
 }
